32f29457ce42705c01db82662c807a68a6f27df7
yahiro <yahiro07@users.noreply.github.com>
2026-06-21 22:09:52 +0900

    add routing dummy unit

--- wafer/src/react/index.ts
+++ wafer/src/react/index.ts
@@ -3,4 +3,5 @@ export * from "./custom-element-unit-frame-fi";
 export * from "./host-app-context";
 export * from "./react-unit-frame";
 export type { ReactUnitTemplateFn } from "./react-unit-interface";
+export * from "./routing-dummy-unit";
 export * from "./unit-frame";
---
+++ wafer/src/react/routing-dummy-unit.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useHostAppContext } from "./host-app-context";
+import {
+  instantiateReactUnit,
+  ReactUnitTemplateFn,
+} from "./react-unit-interface";
+
+type Props = {
+  unitId: string;
+  destSpec?: string;
+};
+
+const routingDummyUnitTemplateFn: ReactUnitTemplateFn = (unitInterface) => {
+  unitInterface.completeSetup({
+    unitAspects: { unitType: "effect" },
+  });
+  return {
+    RenderUi: () => null,
+  };
+};
+
+export const RoutingDummyUnit = ({ unitId, destSpec }: Props) => {
+  const { hostSystem } = useHostAppContext();
+
+  instantiateReactUnit(hostSystem, routingDummyUnitTemplateFn, unitId);
+
+  useEffect(() => {
+    hostSystem.reserveConnectionChange(unitId, destSpec);
+  }, [unitId, destSpec, hostSystem]);
+
+  return null;
+};
