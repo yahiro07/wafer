4b799b96075875cc55858fcedecef23fa8f699db
yahiro <yahiro07@users.noreply.github.com>
2026-08-05 23:53:44 +0900

    add thumbnail capturer example

---
+++ example-apps/unit-thumbnail/src/unit-source-urls.ts
@@ -0,0 +1,11 @@
+export const unitSourceUrls = [
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r16/graphite-drum-machine/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r16/mini-synth-ge/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r16/bseq2/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r16/tonerio-sequencer/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-units@r16/fluorite-piano-roll/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-custom-units@r16/webaudio-tinysynth-mini/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-custom-units@r16/super-oscillator/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wafer-custom-units@r16/vue-audio-mixer/",
+  //"file:///path/to/your/local/unit/to/capture/thumbnail/"
+];
