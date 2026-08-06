de09ff795aebb98e33e72d8c1519b90b7cba7de3
yahiro <yahiro07@users.noreply.github.com>
2026-08-06 09:33:48 +0900

    fix casing

--- example-apps/simple-session/vite.config.ts
+++ example-apps/simple-session/vite.config.ts
@@ -2,12 +2,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { unitLoaderPlugin } from "wafer-host/vite-plugin";
 import { unitSourceUrls } from "./src/unit-source-urls";
-import Unocss from "unocss/vite";
+import UnoCSS from "unocss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    Unocss(),
+    UnoCSS(),
     unitLoaderPlugin({ unitSourceUrls, cacheFolderPath: "../.wafer-cache" }),
   ],
 });
