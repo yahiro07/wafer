2288ac82759c8289d808596f4c0b62523c2f3c32
yahiro <yahiro07@users.noreply.github.com>
2026-08-09 01:08:35 +0900

    show app items

--- example-apps/mdi/src/index.tsx
+++ example-apps/mdi/src/index.tsx
@@ -1,8 +1,50 @@
 import { createRoot } from "react-dom/client";
 import "./setup-twind";
+import { css, cx } from "@twind/core";
+import catalog from "./unit-inventories.json";
+
+type CatalogKey = keyof typeof catalog;
+
+type UnitItem = {
+  unitId: string;
+  catalogKey: CatalogKey;
+  destSpec: string;
+};
+
+const unitItems: UnitItem[] = [
+  { unitId: "drum1", catalogKey: "graphiteDrumMachine", destSpec: "$output" },
+  { unitId: "sequencer1", catalogKey: "tonerioSequencer", destSpec: "synth1" },
+  { unitId: "synth1", catalogKey: "webaudioTinysynthMini", destSpec: "effect1" },
+  { unitId: "effect1", catalogKey: "sunsetDelay", destSpec: "$output" },
+];
+
+const UnitLauncherIcon = ({ unitItem }: { unitItem: UnitItem }) => {
+  const { catalogKey } = unitItem;
+  const { thumbnailUrl } = catalog[catalogKey];
+  return (
+    <div className="w-[200px] h-[100px] bg-gray-500 cursor-pointer">
+      <img className="w-full h-full object-contain" src={thumbnailUrl} alt={catalogKey} />
+    </div>
+  );
+};
+
+const TopSection = () => {
+  return (
+    <div className="bg-gray-400 flex-c gap-4 p-4">
+      {unitItems.map((item) => (
+        <UnitLauncherIcon key={item.unitId} unitItem={item} />
+      ))}
+    </div>
+  );
+};
 
 const App = () => {
-  return <div className="p-4 text-blue-500">hello</div>;
+  return (
+    <div className={cx("flex-v", css({ height: "100dvh" }))}>
+      <TopSection />
+      <div className="bg-gray-200 grow" />
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("app")!;
