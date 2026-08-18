d6a1802c9dcc85ac575e7a921f4b5f6e59998368
yahiro <yahiro07@users.noreply.github.com>
2026-08-18 15:41:53 +0900

    update example projects

--- example-apps/mdi-app/src/unit-source-urls.ts
+++ example-apps/mdi-app/src/unit-source-urls.ts
@@ -1,6 +1,6 @@
 export const unitSourceUrls = [
-  "https://github.com/yahiro07/wafer-units/tree/r21/graphite-drum-machine/",
-  "https://github.com/yahiro07/wafer-units/tree/r21/tonerio-sequencer/",
-  "https://github.com/yahiro07/wafer-units/tree/r21/sunset-delay/",
+  "https://github.com/yahiro07/wafer-units/tree/r22/graphite-drum-machine/",
+  "https://github.com/yahiro07/wafer-units/tree/r22/tonerio-sequencer/",
+  "https://github.com/yahiro07/wafer-units/tree/r22/sunset-delay/",
   "https://github.com/yahiro07/wafer-custom-units/tree/r19/webaudio-tinysynth-mini/",
 ];
